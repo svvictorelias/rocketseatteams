b48e6151edcd6b5b3b6afa2ad3ce0ddee22bce8d
Victor Elias <svvictorelias@gmail.com>
2022-12-17 17:46:31 -0300

    feat: initial

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import "styled-components";
+
+import theme from "@theme/index";
+
+declare module "styled-components" {
+  type ThemeType = typeof theme;
+
+  export interface DefaultTheme extends ThemeType {}
+}
