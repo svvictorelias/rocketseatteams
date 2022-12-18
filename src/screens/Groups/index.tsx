e68aea97ccb1e2cb944f5b671d581fb8dcd4cdaf
Victor Elias <svvictorelias@gmail.com>
2022-12-18 12:13:46 -0300

    feat: add async storage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { FlatList } from "react-native";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
@@ -6,7 +6,8 @@ import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -14,6 +15,23 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate("new");
   }
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
+
   return (
     <Container>
       <Header />
@@ -21,7 +39,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={item => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
---
+++ src/storage/group/groupCreate.ts
@@ -0,0 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GROUP_COLLECTION } from "@storage/storageConfig";
+import { AppError } from "@utils/AppError";
+import { groupsGetAll } from "./groupsGetAll";
+
+export async function groupCreate(newGroup: string) {
+  try {
+    const storageGroups = await groupsGetAll();
+    const groupAlreadyExists = storageGroups.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError("Grupo já cadastrado com esse nome");
+    }
+
+    const storage = JSON.stringify([...storageGroups, newGroup]);
+    await AsyncStorage.setItem(GROUP_COLLECTION, storage);
+  } catch (error) {
+    throw error;
+  }
+}
